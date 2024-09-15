98caf76d794f5640c15b0bf91938eae2bb74f1a7
Vic <piton20091986@gmail.com>
2024-09-15 20:28:07 +0300

    add media

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import OrientationDetector from "../components/OrientationDtect";
+import klipartz from '../media/klipartz.png'
+import coin_o from '../media/coin_o.png'
 
 const HomePage:React.FC = () => {
     const [orientation, setOrientation] = useState<'portrait' | 'landscape'>(
@@ -10,10 +12,27 @@ const HomePage:React.FC = () => {
         setOrientation(newOrientation);
     };
     return (
-        <div>
+        <>
             <OrientationDetector onOrientationChange={handleOrientationChange} />
-            <h1>Current Orientation: {orientation}</h1>
-        </div>
+            {orientation === 'landscape'&&
+                <div className="landscapeDisplay">
+                    <h1>Приложение работает исключительно в портретном режиме</h1>
+                </div>
+            }
+            {orientation === 'portrait'&&
+                <div className="portraitDisplay">
+                    <div className="greenSide">
+                        <div className="coinBox">
+                            <img src={coin_o} alt="pic"></img>
+                        </div>
+                    </div>
+                    <div>
+                        <h3 className="title">Приложение работает</h3>
+                        <img className="App-logo" src={klipartz} alt="pic"/>
+                    </div>
+                </div>
+            }
+        </>
     )
 }
 
